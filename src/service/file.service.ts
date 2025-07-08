aa64e40574c3d6dc69fcc6db3f7c46e4418b02aa
albqvictor1508 <albq.victor@gmail.com>
2025-07-07 23:02:36 -0300

    chore: convert to pdf

--- src/service/file.service.ts
+++ src/service/file.service.ts
@@ -3,11 +3,14 @@ import * as sharp from 'sharp';
 
 @Injectable()
 export class FileService {
-  async convertToWebp(image: Buffer): Promise<Buffer> {
-    return sharp(image).webp({ quality: 80 }).toBuffer();
+  async convertToWebp(imageBuffer: Buffer): Promise<Buffer> {
+    return sharp(imageBuffer).webp({ quality: 80 }).toBuffer();
   }
 
-  //WARN: Procurar formas de converter: SVG, PDF, DOCX. pensando em um MVP, dps posso criar soluções na mão 
-  async convertToSvg(image: Buffer): Promise<Buffer> {
+  //WARN: Procurar formas de converter: SVG, PDF, DOCX. pensando em um MVP, dps posso criar soluções na mão
+  async convertToSvg(imageBuffer: Buffer) {
   }
+
+  //WARN: tem algumas soluções, como cloudConverter e API's de conversão de arquivos
+  async convertToPdf(fileBuffer: Buffer) { }
 }
